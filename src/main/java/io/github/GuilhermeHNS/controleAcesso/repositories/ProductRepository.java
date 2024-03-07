86d3ea921e780e60ea01b34294255753f6f1e469
Guilherme Henrique Novaes de Souza <guilhermehenriquenovaes@gmail.com>
2024-03-06 22:23:23 -0300

    Criado tabela de users Criado entidade de users Adicionado spring security Criado service de authorization

--- src/main/java/io/github/GuilhermeHNS/controleAcesso/repositories/ProductRepository.java
+++ src/main/java/io/github/GuilhermeHNS/controleAcesso/repositories/ProductRepository.java
@@ -2,6 +2,8 @@ package io.github.GuilhermeHNS.controleAcesso.repositories;
 
 import io.github.GuilhermeHNS.controleAcesso.domain.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
 }
---
+++ src/main/java/io/github/GuilhermeHNS/controleAcesso/services/AuthorizationService.java
@@ -0,0 +1,18 @@
+package io.github.GuilhermeHNS.controleAcesso.services;
+
+import io.github.GuilhermeHNS.controleAcesso.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+    @Autowired
+    UserRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
